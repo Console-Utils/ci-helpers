1f3878eeb11ce6b79cc92eaac426d6d063710265
Emily Seville <emilyseville7cf@gmail.com>
2021-10-07 22:21:02 +1000

    Create check-branch-mergeability.sh

---
+++ check-branch-mergeability.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Checks whether futher CI jobs can be run by mandating:
+# - only feature/bugfix branches merged to dev
+# - only dev branch merged to master
+
+declare -ir SUCCESS=0
+declare -ir WRONG_BRANCH=1
+
+case "$GITHUB_BASE_REF" in
+    dev)
+        if [[ $GITHUB_HEAD_REF != @(feature|bugfix)/* ]]; then
+            echo "Source branch must be \"feature/<issue-id>\" or \"bugfix/<issue-id>\" branch \
+when merged to \"dev\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+            exit $WRONG_BRANCH
+        fi
+    ;;
+    master|main)
+        if [[ $GITHUB_HEAD_REF != dev ]]; then
+            echo "Source branch must be \"dev\" branch \
+when merged to \"master\" or \"main\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+            exit $WRONG_BRANCH
+        fi
+    ;;
+    *)
+        echo "Base branch must be \"dev\" or \"master\" branch, but now it is \"$GITHUB_BASE_REF\"." >&2
+        exit $WRONG_BRANCH
+    ;;
+esac
+
+exit $SUCCESS
