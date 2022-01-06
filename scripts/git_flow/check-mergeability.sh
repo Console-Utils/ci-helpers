bc346e5e6cdc9de8c10d6a8a44ef9c92b3702950
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2022-01-07 00:38:27 +1000

    Fix Git flow shell script

--- scripts/git_flow/check-mergeability.sh
+++ scripts/git_flow/check-mergeability.sh
@@ -9,21 +9,21 @@ declare -ir WRONG_BRANCH=1
 
 case "$GITHUB_BASE_REF" in
     dev)
-        if [[ $GITHUB_HEAD_REF != @(feature|bugfix)/* ]]; then
-            echo "Source branch must be \"feature/<issue-id>\" or \"bugfix/<issue-id>\" branch \
-when merged to \"dev\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+        if [[ $GITHUB_HEAD_REF != @(feature|bugfix)/+([[:digit:]]) ]]; then
+            echo "Source branch must be 'feature/<number>'|'bugfix/<number>' branch \
+when merged to 'dev', but now it is '$GITHUB_HEAD_REF'" >&2
             exit $WRONG_BRANCH
         fi
     ;;
     master|main)
         if [[ $GITHUB_HEAD_REF != dev ]]; then
-            echo "Source branch must be \"dev\" branch \
-when merged to \"master\" or \"main\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+            echo "Source branch must be 'dev' branch \
+when merged to 'master'|'main', but now it is '$GITHUB_HEAD_REF'" >&2
             exit $WRONG_BRANCH
         fi
     ;;
     *)
-        echo "Base branch must be \"dev\" or \"master\" or \"main\" branch, but now it is \"$GITHUB_BASE_REF\"." >&2
+        echo "Base branch must be 'dev'|'master'|'main', but now it is '$GITHUB_BASE_REF'." >&2
         exit $WRONG_BRANCH
     ;;
 esac
