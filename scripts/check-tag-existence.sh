45058c929d5fc7dd460ef4fc7e2a60566a8ff105
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2022-01-07 01:05:04 +1000

    Update check-tag-existence: - prefer long options - prefer single quotes

--- scripts/check-tag-existence.sh
+++ scripts/check-tag-existence.sh
@@ -11,7 +11,7 @@ declare -ir NO_TAG_ERROR=1
 
 git checkout HEAD^2
 
-last_tag=$(git tag | sed -En '/v/p' | sort -r | head -n 1)
+last_tag=$(git tag | sed --regexp-extended --quiet '/v/p' | sort --reverse | head --lines 1)
 
 if [[ -z $last_tag ]]; then
     echo "No version tag found." >&2
@@ -22,7 +22,7 @@ current_sha=$(git rev-parse HEAD)
 last_tag_sha=$(git rev-parse "$last_tag")
 
 if [[ "$last_tag_sha" != "$current_sha" ]]; then
-    echo "\"$current_sha\" must have \"$last_tag\", which points now to \"$last_tag_sha\" commit." >&2
+    echo "'$current_sha' must have '$last_tag', which points now to '$last_tag_sha' commit." >&2
     exit $NO_TAG_ERROR
 fi
 
