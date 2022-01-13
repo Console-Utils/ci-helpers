cdecf5d788e38cba020ea60859d8ee5b0ea481c3
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2022-01-13 16:10:28 +1000

    Move tag check to a separate script

---
+++ scripts/check_tag_format.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+declare -ir success="0"
+declare -ir wrong_tag_error="1"
+
+declare tag="$1"
+declare versioning="$2"
+
+case "$versioning" in
+  semantic)
+    declare pattern='v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'
+    ;;
+  *)
+    declare pattern='v[[:digit:]]+\.[[:digit:]]+'
+    ;;
+esac
+
+if [[ "$tag" =~ $pattern ]]; then
+  echo "⛔ Release tag must have $versioning versioning format but not it is '$tag'."
+  exit "$wrong_tag_error"
+fi
+
+echo "✅ Tag is correct."
+exit $success
